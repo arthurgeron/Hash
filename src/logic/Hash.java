7d87eea1749eef22a820bc9adede128494863aa2
Arthur Geron <arthcronos@hotmail.com>
2015-06-19 18:21:53 -0300

    Upgrade
    [ADD] HashTable
    [IMPROVE] Collision rate -> 0,1% Up to 3000 words

--- src/logic/Hash.java
+++ src/logic/Hash.java
@@ -1,49 +1,64 @@
 package logic;
 
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.List;
 
 
-public class Hash {
-	private String[] hashTable;
-	boolean autoIncrement = false;
-	ArrayList<Integer> storedElements = new ArrayList<Integer>();
+public class Hash  {
+	private List<Integer> keys = new ArrayList<Integer>();
+	private List<String> info = new ArrayList<String>();
+	
 	public Hash() {
-		// TODO Auto-generated constructor stub
-		autoIncrement = true;
-	}
-	
-	public Hash(int arraySize) {
-		hashTable = new String[arraySize];
 	}
 	
 	
-	private String getElementAtPosition(int position){
-			return hashTable[position];
-	}
+
 	
 	public boolean storeInfo(String info)//Generate list position from hashCode and store it
 	{
 		int indexLocation;
 		try{
 			indexLocation = hashCode(info);
-			
-			if(hashTable == null){
-				hashTable = setNewSizeForStringArray(hashTable,indexLocation+1);
-			}
-			if(hashTable.length<	indexLocation+1){
-				if(autoIncrement){
-					hashTable = setNewSizeForStringArray(hashTable,indexLocation+1);
+			if(keys.contains(indexLocation)){
+				if(!keys.contains(hashCode(String.valueOf(indexLocation))))
+				{
+					keys.add(indexLocation);
+					this.info.add(info);
 				}
-				else {
-					return false;
-				}
-			}
-			
-			if(hashTable[indexLocation] != null) {
 				return false;
 			}
-			hashTable[indexLocation] = info;
-			return true;
+			else{
+				keys.add(indexLocation);
+				this.info.add(info);
+				return true;
+			}
+		}
+		catch(Exception exception) {
+			return false;
+		}
+	}
+	
+	
+	public boolean storeInfoUsinAdler32(String info)//Generate list position from hashCode and store it
+	{
+		int indexLocation;
+
+		try{
+			indexLocation = hashCode2(info);
+			if(keys.contains(indexLocation)){
+				return false;
+			}
+			else{
+				keys.add(indexLocation);
+				this.info.add(info);
+				return true;
+			}
 		}
 		catch(Exception exception) {
 			return false;
@@ -51,82 +66,134 @@ public class Hash {
 	}
 	
 	public void drawTable(){
-		int nonNullElements = 0;
-		for(int i = 0 ; i < hashTable.length ; i++) {
-			if(hashTable[i]!=null) {
-				nonNullElements++;
-				System.out.println("Position: "+i+" Value: "+hashTable[i]);
-			}
+
+		for(Integer hash : keys) {
+			
+				System.out.println("Position: "+keys.indexOf(hash)+" Value: "+info.get(keys.indexOf(hash)));
 			
 		}
-		System.out.println("Number of valid elements: "+nonNullElements);
+		System.out.println("Number of valid elements: "+keys.size());
 	}
 	
-	private String[] setNewSizeForStringArray(String[] firstArray, int newSize){
-		String[] result = new String[newSize];
-		try{
-		for(int i = 0; i < firstArray.length; i++){
-				result[i] = firstArray[i];
-			}
-			System.gc();//Garbage collector call
-			return result;
-		}
-		catch(Exception exception) {
-			System.gc();//Garbage collector call
-			return null;
-		}
-	}
-	
-	public String findInfo(String info)//Find info from list using hashCode
+	public boolean containInfo(String info)//Find info from list using hashCode
 	{
-		try{
 			int indexLocation = hashCode(info);
-			return getElementAtPosition(indexLocation);
-		}
-		catch(Exception exception) {
-			return null;
-		}
+			if(keys.contains(indexLocation) && this.info.get(keys.indexOf(indexLocation)).equals(info)){
+				return true;
+			}
+			else {
+				return false;
+			}
 	}
 	
 	private int hashCode(String info) {//Alterar para utilizar bits
-		  int result = 0, counter=0,countTries=0;
+		  int result = 1,auxiliar = 0;
+		  int base = 1759;
 		  for(int i =0; i < info.length() ; i++) {
-			  result += (int)info.charAt(i);
-			  if(counter==0){
-				  result += (int)info.charAt(i) + info.length() + (i+1);
-			  	counter++;
-			  }
-			  else if(counter==1) {
-				  result *= (int)info.charAt(i) * (info.length()/4) ;
-				  counter++;
-			  }
-			  else if(counter==2){
-				  result -= (int)info.charAt(i) - info.length() - (i+2);
-				  	counter++;  
-			  }
-			  else {
-				  result /= (int)info.charAt(i) / info.length();
-				  counter = 0;
-			  }
+			 
+			 // base = base  + i; // Low numbers method
+			  
+			  base += base  + i; // High numbers method
+			  
+			  result += (info.charAt(i) & 0xFF) + info.length() + (i+1);
+			  result += (info.charAt(i) & 0xFF) + info.length() + (i+1);
+			  result *= (info.charAt(i) & 0xFF) * (info.length()/4) ;
+			  result -= ( (info.charAt(i) & 0xFF) - info.length() - (i+2) );
+			  
 			  if(i+1<=info.length()-1){
-				  result += ((int)info.charAt(i) - (int)info.charAt(i+1));
+				  result += ((info.charAt(i) & 0xFF) - (int)info.charAt(i+1));
 			  }
 			  else if(i-1 > 0){
-				  result += ((int)info.charAt(i) + (int)info.charAt(i-1));
+				  result += ((info.charAt(i) & 0xFF) + (int)info.charAt(i-1));
 			  }
-			  
+			  result = (result + (info.charAt(i) & 0xFF) ) % base;
+			  auxiliar = (result + auxiliar) % base;
 		  }
 		  result = result < 0 ? Math.abs(result) : result;
-		  while(result>hashTable.length - 1 ) {
-			  result =  (result/2) + (result%2);
-			  result = result < 0 ? Math.abs(result) : result;
-		  }
-		  while(hashTable[result] != null && countTries != hashTable.length){
-			  countTries++;
-				result = result - String.valueOf(result).length() - (String.valueOf(result).length()%2);
-			}
+		  result += auxiliar;
+		  result = result < 0 ? Math.abs(result) : result;
 		  return result;
+	}
+	
+	public void readAndTestFile(String fileLocation) throws FileNotFoundException{
+		BufferedReader reader = new BufferedReader(new FileReader(fileLocation));
+		String currentLine;
+		String[] words;
+		int collisionCounter=0,wordsCounter=0;
+		Scanner scanner = new Scanner(System.in);
+		
+		try {
+			while ( reader.ready())
+			{
+				currentLine = reader.readLine();
+				words = currentLine.split("\\s");
+				for(int i = 0 ; i < words.length ; i++) {
+					if(storeInfo(words[i])==false){
+						collisionCounter++;
+					}
+					wordsCounter++;
+				}
+			}
+			reader.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		
+		System.out.println("Total de palavras utilizando o novo método: "+wordsCounter+", porcentual de falhas: "+ ((collisionCounter*100)/Double.valueOf(wordsCounter))+"%.");
+		
+		System.out.println("Você deseja visualizar a tabela hash?");
+		switch(scanner.next().toLowerCase()){
+			case"sim":
+				drawTable();
+				break;
+		}
+		scanner.close();
+	}
+	
+	public void readAndTestFileUsingAdler32(String fileLocation) throws FileNotFoundException{
+		BufferedReader reader = new BufferedReader(new FileReader(fileLocation));
+		String currentLine;
+		Hash hash = this;
+		String[] words;
+		int collisionCounter=0,wordsCounter=0;
+		Scanner scanner = new Scanner(System.in);
+		
+		try {
+			while ( reader.ready())
+			{
+				currentLine = reader.readLine();
+				words = currentLine.split("\\s");
+				for(int i = 0 ; i < words.length ; i++) {
+					if(hash.storeInfoUsinAdler32(words[i])==false){
+						collisionCounter++;
+					}
+					wordsCounter++;
+				}
+			}
+			reader.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println("Total de palavras utilizando Adler32: "+wordsCounter+", porcentual de falhas: "+ ((collisionCounter*100)/Double.valueOf(wordsCounter))+"%.");
+		
+		scanner.close();
+	}
+	
+	private int hashCode2(String info) {//Adler32
+		  int base,result = 0, a=0,b;
+		  a = 1;
+		  b = 0;
+		  base = 65521;
+		  for(int i =0; i < info.length() ; i++) {
+			  a = (a + (info.charAt(i) & 0xFF)) % base;
+			  b = (a + b) % base;
+		  }
+		  result = (b << 16) | a;
+		  return result;
+	}
 	
 
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -10,46 +10,36 @@ import logic.Hash;
 
 public class Main {
 
+
+	
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated constructor stub
-		Hash hash = new Hash(1000);
+		
+		
 		BufferedReader reader;
-		int colisionCounter = 0, wordsCounter=0;
-		String currentLine;
-		String[] words;
+		int hashSize;
+		Hash hash;
+		String fileLocation;
 		Scanner scanner;
-		while(true) {
 			try{
 				scanner = new Scanner(System.in);
 				System.out.println("Digite o local do arquivo de texto:");
-				reader = new BufferedReader(new FileReader(scanner.next().replace("\"", "")));
-				while ( reader.ready())
-				{
-					currentLine = reader.readLine();
-					words = currentLine.split("\\s");
-					for(int i = 0 ; i < words.length ; i++) {
-						if(hash.storeInfo(words[i])==false){
-							colisionCounter++;
-						}
-						wordsCounter++;
-						//System.out.println("Total words: "+wordsCounter+" | Total collisions: " + colisionCounter);
-					}
-				}
-				System.out.println("Total de palavras: "+wordsCounter+", porcentual de falhas: "+ Math.round((colisionCounter*100)/Double.valueOf(wordsCounter))+"%.");
-				System.out.println("Você deseja visualizar a tabela hash?");
-				switch(scanner.next().toLowerCase()){
-					case"sim":
-						hash.drawTable();
-						break;
-				}
-				scanner.close();
-				break;
+				fileLocation = scanner.next().replace("\"", "");
+				hashSize = (int) new BufferedReader(new FileReader(fileLocation)).lines().count() * 100;
+				reader = new BufferedReader(new FileReader(fileLocation));
+				
+				hash = new Hash();
+				
+				hash.readAndTestFile(fileLocation);
+				
+				hash = new Hash();
+				
+				hash.readAndTestFileUsingAdler32(fileLocation);
+				
 			}
 			catch(Exception exception) {
 				System.out.println(exception.toString());
-				break;
 			}
-		}
 		
 	}
 
